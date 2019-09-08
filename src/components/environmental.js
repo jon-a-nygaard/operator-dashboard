f3c37e92eae01d8a3e3c0b94ba640e9809406231
jon-a-nygaard <jon.arild@highsoft.com>
2019-09-08 17:12:56 +0200

    Add environmental data component

---
+++ src/components/environmental.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Environmental extends React.Component {
+    render() {
+        return <div class="row">
+            <div class="col">
+                <h2>Environmental data</h2>  
+            </div> 
+        </div>
+    }
+}
